2af42974a69587c34bb2b3f92a2d882ea61a01cc
Pieter Pas <pieter.pas@outlook.com>
2020-01-22 00:36:06 +0100

    GNU make 4.3

--- toolchain/dockerx/aarch64-installed-tools-x/build.sh
+++ toolchain/dockerx/aarch64-installed-tools-x/build.sh
@@ -4,8 +4,9 @@ set -ex
 
 # docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
 # docker buildx rm testbuilder || true
-# docker buildx create --name testbuilder --driver docker-container
+# docker buildx create --name testbuilder --platform linux/arm64
 # docker buildx use testbuilder
-# docker buildx inspect testbuilder --bootstrap
+../../../scripts/install-docker-binfmt.sh
+docker buildx inspect --bootstrap
 docker buildx build --platform linux/arm64 . --load -t aarch64-installed-tools-x
 # docker buildx rm testbuilder
