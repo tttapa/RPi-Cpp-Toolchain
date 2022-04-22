00ff54302d619cfb85f7e41bb7507710ddad8f1c
Pieter Pas <pieter.pas@outlook.com>
2022-04-22 23:34:07 +0200

    Upgrade Git

--- toolchain/docker/merged/cross-build/install-scripts/git.sh
+++ toolchain/docker/merged/cross-build/install-scripts/git.sh
@@ -4,13 +4,13 @@ set -ex
 
 # Download
 version=2.29.3
-URL="https://mirrors.edge.kernel.org/pub/software/scm/git/git-$version.0.tar.gz"
+URL="https://mirrors.edge.kernel.org/pub/software/scm/git/git-$version.tar.gz"
 pushd "${DOWNLOADS}"
 wget -N "$URL"
 popd
 
 # Extract
-tar xzf "${DOWNLOADS}/git-$version.0.tar.gz"
+tar xzf "${DOWNLOADS}/git-$version.tar.gz"
 
 # Help Git find cURL
 mkdir /tmp/curl-config
@@ -18,15 +18,17 @@ ln -s "${RPI_SYSROOT}/usr/local/bin/curl-config" /tmp/curl-config/
 export PATH="/tmp/curl-config:${PATH}"
 
 # Configure
-pushd git-$version.0
+pushd git-$version
 . cross-pkg-config
 make configure
-patch configure ../git-$version.0.patch
 ./configure \
     --with-curl \
     --with-openssl \
     --host="${HOST_TRIPLE}" \
     --prefix="/usr/local" \
+    ac_cv_iconv_omits_bom=no \
+    ac_cv_fread_reads_directories=yes \
+    ac_cv_snprintf_returns_bogus=no \
     CFLAGS="--sysroot ${RPI_SYSROOT} -O3 \
             -I${RPI_SYSROOT}/usr/local/include"
 
@@ -38,4 +40,4 @@ make install DESTDIR="${RPI_STAGING}"
 
 # Cleanup
 popd
-rm -rf git-$version.0
+rm -rf git-$version
